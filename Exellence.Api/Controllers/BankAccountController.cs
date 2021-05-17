1f03b34c9db11b89634630e9dcbea9642942abab
Roman Stepanov <ronan1810@gmail.com>
2021-05-17 23:23:27 +0300

    updates

---
+++ Exellence.Api/Controllers/BankAccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Excellence.BL;
+using Excellence.Core.Domain;
+
+namespace Exellence.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankAccountController : ControllerBase
+    {
+        private IAccountService _accountService;
+
+        public BankAccountController(IAccountService accountService)
+        {
+            this._accountService = accountService;
+        }
+        // POST api/<BankAccountController>
+        [HttpPost]
+        public void Post([FromBody] BankAccountInfo requestBankAccountInfo)
+        {
+            _accountService.CreateAsync(requestBankAccountInfo);
+        }
+    }
+}
---
+++ Exellence.Api/Controllers/BankController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Excellence.BL;
+using Excellence.Core.Domain;
+
+namespace Exellence.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankController : ControllerBase
+    {
+        private IBankService _bankService;
+
+        public BankController(IBankService bankService)
+        {
+            this._bankService = bankService;
+        }
+        
+        [HttpGet]
+        public async Task<List<BankInfo>> GetBanksAsync()
+        {
+            var model = await _bankService.GetBanksAsync();
+            return model.ToList();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<List<BankBranchesInfo>> GetBranchesAsync(int id)
+        {
+            var branches = await _bankService.GetBranchesAsync(id);
+            return branches.ToList();
+        }
+    }
+}
